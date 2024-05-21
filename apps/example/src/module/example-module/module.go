99797350ac7a7b9c6c35495d849fddbb6d2db508
phongthien <nguyentruonggiangptdq@gmail.com>
2024-05-21 13:43:38 +0700

    fix: add service

--- apps/example/src/module/example-module/module.go
+++ apps/example/src/module/example-module/module.go
@@ -2,6 +2,8 @@ package example_http
 
 import (
 	"example/src/module/example-module/controller"
+	"example/src/module/example-module/repository"
+	"example/src/module/example-module/service"
 
 	"github.com/sigmaott/gest/package/extension/echofx"
 	"go.uber.org/fx"
@@ -12,11 +14,11 @@ func Module() fx.Option {
 		fx.Provide(
 			echofx.AsRoute(controller.NewExampleController),
 		),
-		// fx.Provide(
-		// 	service.NewExampleService,
-		// ),
-		// fx.Provide(
-		// 	repository.NewExampleRepository,
-		// ),
+		fx.Provide(
+			service.NewExampleService,
+		),
+		fx.Provide(
+			repository.NewExampleRepository,
+		),
 	)
 }
--- apps/example/src/module/example-module/service/service.go
+++ apps/example/src/module/example-module/service/service.go
@@ -1,1 +1,11 @@
 package service
+
+type IExampleService interface {
+}
+type exampleService struct {
+}
+
+func NewExampleService() IExampleService {
+	return &exampleService{}
+
+}
